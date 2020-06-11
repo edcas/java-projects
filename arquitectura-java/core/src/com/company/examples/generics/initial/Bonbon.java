4bc3f7a79069cf18c94c141fea4dffaf03fc281b
Eduard Castro <eduard.cas07@gmail.com>
2020-06-10 20:48:06 -0500

    Add initial generics example

---
+++ arquitectura-java/core/src/com/company/examples/generics/initial/Bonbon.java
@@ -0,0 +1,14 @@
+package com.company.examples.generics.initial;
+
+public class Bonbon {
+
+    private String type;
+
+    public Bonbon(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
