d4cb5518a89548de4bb46ea2612ff3951ff89de4
Eduard Castro <eduard.cas07@gmail.com>
2020-07-05 18:38:01 -0500

    Add Math example - Java API Core

---
+++ arquitectura-java/core/src/com/company/examples/core/math/Main.java
@@ -0,0 +1,14 @@
+package com.company.examples.core.math;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("PI: " + Math.PI);
+        System.out.println("Pow: " + Math.pow(2, 2));
+        System.out.println("Round: " + Math.round(2.34));
+        System.out.println("SQRT: " + Math.sqrt(16));
+
+    }
+
+}
