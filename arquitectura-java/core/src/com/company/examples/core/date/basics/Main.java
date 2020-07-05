0243c920aced976f668060ce6ef7855095e7d008
Eduard Castro <eduard.cas07@gmail.com>
2020-07-05 18:38:17 -0500

    Add Date example - Java API Core

---
+++ arquitectura-java/core/src/com/company/examples/core/date/basics/Main.java
@@ -0,0 +1,18 @@
+package com.company.examples.core.date.basics;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Date dateOne = new Date();
+        System.out.println(dateOne);
+
+        Date dateTwo = new Date(0);
+        System.out.println(dateTwo);
+
+    }
+
+}
