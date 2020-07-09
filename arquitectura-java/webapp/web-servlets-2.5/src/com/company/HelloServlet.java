a987c6cbcdf71959c95e98b4f08b82dd5df31f3a
Eduard Castro <eduard.cas07@gmail.com>
2020-07-09 10:03:54 -0500

    Update web example - arquitecturajava

---
+++ arquitectura-java/webapp/web-servlets-2.5/src/com/company/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
