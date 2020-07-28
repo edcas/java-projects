b6a77e377f3bfa883b9338321de369c84954fe40
Eduard Castro <eduard.cas07@gmail.com>
2020-07-28 06:53:45 -0500

    Add synchronized Thread example

---
+++ arquitectura-java/core/src/main/java/com/company/examples/threads/synchronize/problem/Job.java
@@ -0,0 +1,38 @@
+package com.company.examples.threads.synchronize.problem;
+
+public class Job implements Runnable {
+
+    private Employee employee;
+
+    private int days;
+
+    public Job(Employee employee, int days) {
+        this.employee = employee;
+        this.days = days;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public int getDays() {
+        return days;
+    }
+
+    public void setDays(int days) {
+        this.days = days;
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < days; i++) {
+            System.out.println(employee.incrementDays());
+        }
+
+    }
+}
