58a421e0b0589899287721b7d734ce117fbaf28d
Eduard Castro <eduard.cas07@gmail.com>
2020-08-02 18:31:24 -0500

    Add input hidden - servlets

---
+++ arquitectura-java/webapp/web-servlets-state/src/com/company/inputHidden/InputHiddenServlet.java
@@ -0,0 +1,31 @@
+package com.company.inputHidden;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/InputHiddenServlet")
+public class InputHiddenServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		PrintWriter pw = response.getWriter();
+		pw.println("<html>");
+		pw.println("<body>");
+		pw.println("<form method='post' action='ReaderInputHiddenServlet'>");
+		pw.println("<input type='hidden' name='name' value='" + request.getParameter("name") + "'>");
+		pw.println("Age: <input type='text' name='age'/>");
+		pw.println("<input type='submit' value='submit'>");
+		pw.println("<html>");
+
+	}
+
+}
---
+++ arquitectura-java/webapp/web-servlets-state/src/com/company/inputHidden/ReaderInputHiddenServlet.java
@@ -0,0 +1,25 @@
+package com.company.inputHidden;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ReaderInputHiddenServlet")
+public class ReaderInputHiddenServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		PrintWriter pw = response.getWriter();
+		pw.println(request.getParameter("name"));
+		pw.println(request.getParameter("age"));
+
+	}
+
+}
