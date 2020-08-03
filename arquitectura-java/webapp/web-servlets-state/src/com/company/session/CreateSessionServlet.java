0bd9ccaddffa8302b1ae43bea18926b3ea3eba8d
Eduard Castro <eduard.cas07@gmail.com>
2020-08-03 07:48:56 -0500

    Add simple session example

---
+++ arquitectura-java/webapp/web-servlets-state/src/com/company/session/CreateSessionServlet.java
@@ -0,0 +1,29 @@
+package com.company.session;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/CreateSessionServlet")
+public class CreateSessionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		HttpSession session = request.getSession(true);
+		// setAttribute
+		session.setAttribute("course", "java web");
+
+		PrintWriter pw = response.getWriter();
+		pw.println("session created");
+		pw.close();
+	}
+
+}
