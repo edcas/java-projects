5f5a9b8ad3545d3dab5d6df70018306acb44d6e6
Eduard Castro <eduard.cas07@gmail.com>
2020-08-03 07:49:40 -0500

    Add session context example

---
+++ arquitectura-java/webapp/web-servlets-state/src/com/company/context/ReaderContextServlet.java
@@ -0,0 +1,27 @@
+package com.company.context;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ReaderContextServlet")
+public class ReaderContextServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		ServletContext context = getServletContext();
+
+		PrintWriter pw = response.getWriter();
+		pw.println(context.getAttribute("name"));
+
+	}
+
+}
