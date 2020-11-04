04a1eafcd9b64c3458055c5c8d5f8249206327ef
Eduard Castro <eduard.cas07@gmail.com>
2020-11-04 09:51:43 -0500

    Add API Rest CRUD of people - Version 2

---
+++ arquitectura-java/webinar-springboot/src/main/java/com/company/rest/PersonRestServiceV2.java
@@ -0,0 +1,65 @@
+package com.company.rest;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/people/v2")
+public class PersonRestServiceV2 {
+
+    private static final List<Person> people = new ArrayList<>();
+
+    static {
+        people.add(new Person("John", "Doe", 20));
+        people.add(new Person("Maggie", "Doe", 18));
+    }
+
+    @GetMapping
+    public List<Person> all() {
+        return people;
+    }
+
+    @GetMapping("/{firstName}")
+    public Person find(@PathVariable String firstName) {
+        return people.stream().filter(person -> person.getFirstName().equals(firstName)).findFirst().orElse(null);
+    }
+
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "/people/{firstName}")
+    public void delete(@PathVariable String firstName) {
+        people.removeIf(person -> person.getFirstName().equals(firstName));
+    }
+
+    @PostMapping(value = "/")
+    public ResponseEntity<Person> create(@RequestBody Person person, UriComponentsBuilder builder) {
+        people.add(person);
+
+        HttpHeaders headers = new HttpHeaders();
+        UriComponents location = builder.path("/people/{firstName}").buildAndExpand(person.getFirstName());
+        headers.setLocation(location.toUri());
+
+        return new ResponseEntity<>(person, headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/{firstName}")
+    public ResponseEntity<Void> update(@PathVariable String firstName, @RequestBody Person person) {
+        int index = people.indexOf(new Person(firstName));
+
+        if (index != -1) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        people.set(index, person);
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+}
