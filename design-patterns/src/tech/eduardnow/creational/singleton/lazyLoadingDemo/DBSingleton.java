dcc22d016801964ac189c0e205a4a524972dd43a
Eduard Castro <eduard.cas07@gmail.com>
2021-05-17 20:18:53 -0500

    Add lazy loading singleton - Design Patterns

---
+++ design-patterns/src/tech/eduardnow/creational/singleton/lazyLoadingDemo/DBSingleton.java
@@ -0,0 +1,18 @@
+package tech.eduardnow.creational.singleton.lazyLoadingDemo;
+
+public class DBSingleton {
+
+    private static DBSingleton instance = null;
+
+    private DBSingleton() {
+    }
+
+    public static DBSingleton getInstance() {
+        if (instance == null) {
+            instance = new DBSingleton();
+        }
+
+        return instance;
+    }
+
+}
